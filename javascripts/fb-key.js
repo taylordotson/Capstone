fd8029e85229f1f69a682208f9bae6296aa4e538
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-03-21 13:52:27 -0500

    added fb-config

---
+++ javascripts/fb-key.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function getkey() {
+    return {
+        apiKey: "AIzaSyB93yUrNVSF_LTpMzdW7APVDlJH_5OGnYE",
+        authDomain: "Capstone.firebaseapp.com",
+
+        databaseURL: "https://capstone-4be01.firebaseio.com/"
+    };
+}
+module.exports = getkey;
